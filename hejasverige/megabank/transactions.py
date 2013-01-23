4b33ded86fd7bcafa4f31871e52ec441dddb3715
Daniel Grindelid <daniel.grindelid@gmail.com>
2013-01-23 11:26:30 +0100

    Errorhandling

--- hejasverige/megabank/transactions.py
+++ hejasverige/megabank/transactions.py
@@ -5,6 +5,7 @@ from plone import api
 from DateTime import DateTime
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from hejasverige.megabank import _
+import logging
 
 #from StringIO import StringIO
 
@@ -41,14 +42,18 @@ class ListTransactionsView(grok.View):
         >>> self.settings.megabank_password
         >>> ''
         '''
+        logger = logging.getLogger("@@list-transactions")
 
         import requests
-        import logging
         import re
         import json
         import datetime
         # from lxml import etree
         from requests.auth import HTTPBasicAuth
+        #from requests import TimeoutError
+        from requests.exceptions import ConnectionError
+        from requests.exceptions import Timeout
+
         from zope.component import getUtility
         from plone.registry.interfaces import IRegistry
         registry = getUtility(IRegistry)
@@ -63,16 +68,19 @@ class ListTransactionsView(grok.View):
         pid = user.getProperty('personal_id')
 
         self.hasTransactions = True
+        self.hasConnectionError = False
 
         today = DateTime().strftime('%Y-%m-%d %H:%M:%S')
         self.now = today #api.portal.get_localized_time(datetime=today)
+
+        # verify that the megabank settings are made before requesting...
+
 
         if pid:
 
             url = mburl + '/transactions/' + str(pid)
 
 
-            logger = logging.getLogger("@@list-transactions")
             logger.info('Check user account (' + str(user)+ ')')        
             logger.info('Calling MegaBank')
             logger.info('User: ' + mbuser)
@@ -81,35 +89,50 @@ class ListTransactionsView(grok.View):
 
             auth = HTTPBasicAuth(mbuser, mbpassword)
 
-            r = requests.get(url, auth=auth)
-            #f = StringIO(r.text)
-            #tree = etree.parse(f)
-            if r.text:
-                payload = json.loads(r.text)
+            self.hasConnectionError = False
+            try:
+                r = requests.get(url, auth=auth, timeout=5.000)
 
-                #dct = json.loads(r.json, object_hook=datetime_parser)
-                print r.text
+                #f = StringIO(r.text)
+                #tree = etree.parse(f)
+                if r.text:
+                    payload = json.loads(r.text)
 
-                items = []
-                for item in payload:
-                    for k in item.keys():
-                        p = re.compile('/Date\(')
-                        m = p.match(str(item[k]))
-                        if m:
-                            #item[k] = str(self.decode_json_date(item[k]))
-                            item[k] = datetime.datetime(1970, 1, 1) + \
-                                datetime.timedelta(milliseconds=int(re.findall(r'\d+', item[k])[0])) + \
-                                datetime.timedelta(hours=int(re.findall(r'\d+', item[k])[1][:2]))
+                    #dct = json.loads(r.json, object_hook=datetime_parser)
+                    print r.text
 
-                    logger.info('Current item: ' + str(item['ID']))
-                    items.append(item)
+                    items = []
+                    for item in payload:
+                        for k in item.keys():
+                            p = re.compile('/Date\(')
+                            m = p.match(str(item[k]))
+                            if m:
+                                #item[k] = str(self.decode_json_date(item[k]))
+                                item[k] = datetime.datetime(1970, 1, 1) + \
+                                    datetime.timedelta(milliseconds=int(re.findall(r'\d+', item[k])[0])) + \
+                                    datetime.timedelta(hours=int(re.findall(r'\d+', item[k])[1][:2]))
+
+                        logger.info('Current item: ' + str(item['ID']))
+                        items.append(item)
 
 
-                self.transactions =  items
-            else:
+                    self.transactions =  items
+                else:
+                    logger.info("No transactions in payload")
+                    self.hasTransactions = False
+            except ConnectionError:
                 self.hasTransactions = False
+                self.hasConnectionError = True
+                logger.info("Connection Error")
+            except Timeout:
+                self.hasTransactions = False
+                self.hasConnectionError = True
+                logger.info("Timeout")
+
+
         else:
             self.hasTransactions = False
+            logger.info("User %s has no personal_id", str(user))
 
         #import pdb ; pdb.set_trace()
 
